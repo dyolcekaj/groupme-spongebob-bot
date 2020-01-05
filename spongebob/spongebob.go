d2e7c42c5aa5bf33aada98735a3607c8c301c0e6
dyolcekaj@gmail.com <dyolcekaj@gmail.com>
2020-01-04 18:34:14 -0600

    Add spongebob specific commands

---
+++ spongebob/spongebob.go
@@ -0,0 +1,58 @@
+package spongebob
+
+import (
+	"github.com/dyolcekaj/groupme-spongebob-bot/groupme"
+	"unicode"
+)
+
+var _ groupme.Command = &PlainTextSarcasm{}
+type PlainTextSarcasm struct {
+
+}
+
+func (c *PlainTextSarcasm) Name() string {
+	return "PlainTextSarcasm"
+}
+
+func (c *PlainTextSarcasm) Matches(text string) bool {
+	return false
+}
+
+func (c *PlainTextSarcasm) Execute(msg groupme.Message, client *groupme.Client) error {
+	return nil
+}
+
+var _ groupme.Command = &LastMessageSarcasm{}
+type LastMessageSarcasm struct {
+
+}
+
+func (c *LastMessageSarcasm) Name() string {
+	return "PlainTextSarcasm"
+}
+
+func (c *LastMessageSarcasm) Matches(text string) bool {
+	return false
+}
+
+func (c *LastMessageSarcasm) Execute(msg groupme.Message, client *groupme.Client) error {
+	return nil
+}
+
+func translateText(text string) string {
+	var ret []rune
+	i := false
+	for _, r := range text {
+		if i {
+			ret = append(ret, unicode.ToUpper(r))
+		} else {
+			ret = append(ret, unicode.ToLower(r))
+		}
+
+		if unicode.IsLetter(r) {
+			i = !i
+		}
+	}
+
+	return string(ret)
+}
